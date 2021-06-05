6778cd209528484e7844a142e1d09a04423332a2
Gunnar Ulatowski <gunnarulatowski@outlook.com>
2021-06-04 22:19:22 -0400

    Added arrays and more conditional statements

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,17 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
+
   // WHEN I click the button to generate a password
+
   // THEN I am presented with a series of prompts for password criteria
+
     // I need a prompt asking password length (solved: mini project)
+
     var passwordLength = window.prompt("Enter a length between 8-128 characters")
 
         // conditional statement checks number of characters
+
+        var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+        var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+        var upperCase = ['D', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+        var specialChar = ['-', '_', '=', '+', '/', '*', '(', ')', '&', '^', '%', '$', '#', '@', '!', '<', '>', '{', '}', '[', ']', ';', ':'];
+
         var charSet = "";
-        
-        if( ) {
-          charSet = "0123456789";
+
+var charsToUse = [];
+
+        var hasNums = confirm("Do you want numbers?");
+        if(hasNums === true ) {
+          charSet = "";
         }
+
+        var hasLower = confirm("Do you want lowercase characters?");
+        if(hasLower === true ) {
+          charSet = "";
+        }
+
+        var hasUpper = confirm("Do you want uppercasecase characters?");
+        if(hasUpper === true ) {
+          charSet = "";
+        }
+
+        var hasSpecial = confirm("Do you want lowercase Characters?");
+        if(hasSpecial === true ) {
+          charSet = "";
+        }
+
 
     // I need 4 confirms for uppercase, lowercase, special characters, and numbers
       // Need for arrays that house these characters
